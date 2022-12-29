cf432ae63527a268a38bc9b21769efb6cdeae26d
k-sang-soo <wsckm1@google.com>
2022-12-29 22:03:31 +0900

    router 설정 및 header 제작 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Routes/Components/Header';
+import Home from './Routes/Home';
+import Search from './Routes/Search';
+import Tv from './Routes/TV';
 
 function App() {
-    return <div>Hello</div>;
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/tv" element={<Tv />} />
+                <Route path="/search" element={<Search />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
