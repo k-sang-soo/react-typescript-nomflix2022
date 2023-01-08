79465bc4fca0ff6c98216d61bfd085fa0fa631de
k-sang-soo <wsckm1@google.com>
2023-01-08 22:40:46 +0900

    슬라이드 반응형 일부 제작, 슬라이드 타이틀 추가, 클릭 시 페이지 url 변경 추가

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 import { getMovies, IGetMoviesResult } from './../api';
 import { makeImagePath } from './../until';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useMatch, useNavigate } from 'react-router-dom';
 
 const Wrapper = styled.section`
     background-color: #000;
@@ -27,6 +28,8 @@ const Banner = styled.div<{ bgPhoto: string }>`
     height: calc(100vh - 65px);
     padding: 0 60px;
     background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(${(props) => props.bgPhoto});
+    background-position: center center;
+    background-repeat: no-repeat;
     background-size: cover;
 `;
 
@@ -45,44 +48,81 @@ const Slider = styled.div`
     top: -100px;
 `;
 
-const Row = styled(motion.div)`
+const Row = styled(motion.div)<{ $grid: number }>`
     position: absolute;
     display: grid;
     gap: 10px;
-    grid-template-columns: repeat(6, 1fr);
+    grid-template-columns: repeat(${(props) => (props.$grid === 6 ? 6 : props.$grid === 4 ? 4 : props.$grid === 3 && 3)}, 1fr);
     width: 100%;
 `;
 
 const Box = styled(motion.div)<{ $bgPhoto: string }>`
     height: 0;
     padding-bottom: 56.2%;
-    color: red;
-    font-size: 24px;
     background-color: #fff;
     background-image: url(${(props) => props.$bgPhoto});
     background-position: center center;
     background-repeat: no-repeat;
     background-size: cover;
+    cursor: pointer;
+    &:first-child {
+        transform-origin: center left;
+    }
+
+    &:last-child {
+        transform-origin: center right;
+    }
 `;
 
-console.log('Box', Row);
+const Info = styled(motion.div)`
+    padding: 10px;
+    background-color: ${(props) => props.theme.black.lighter};
+    opacity: 0;
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    h4 {
+        text-align: center;
+        font-size: 18px;
+    }
+`;
 
-const rowVariants = {
-    hidden: {
-        x: window.innerWidth,
+const boxVariants = {
+    normal: {
+        scale: 1,
+        y: 0,
     },
-    visible: {
-        x: 0,
-    },
-    exit: {
-        x: -window.innerWidth,
+    hover: {
+        scale: 1.3,
+        y: -50,
+        transition: {
+            delay: 0.5,
+            type: 'tween',
+        },
     },
 };
 
-const offset = 6;
+const infoVariants = {
+    hover: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duaration: 0.1,
+            type: 'tween',
+        },
+    },
+};
 
 function Home() {
+    //고쳐야될 버그 : resize 후 슬라이드 시 width 가 달라지면서 넘길 때 오류가 생김
+    // increaseIndex 함수에서 seIndex가 실행되기 때문인지 확인 필요
+    const navigate = useNavigate();
+    const moviePopupMatch = useMatch('/movies/:movieId');
+    console.log('moviePopupMatch', moviePopupMatch);
+
+    const [slideGrid, setSlideGrid] = useState(0);
     const { data, isLoading } = useQuery<IGetMoviesResult>(['movies', 'nowPlaying'], getMovies);
+    const [winW, setWinW] = useState(window.innerWidth);
     const [index, setIndex] = useState(0);
     const [leaving, setLeaving] = useState(false);
     const increaseIndex = () => {
@@ -90,13 +130,53 @@ function Home() {
             if (leaving) return;
             setLeaving(true);
             const totalMoviesLength = data.results.length - 1;
-            const maxIndex = Math.floor(totalMoviesLength / offset) - 1;
+            const maxIndex = Math.floor(totalMoviesLength / slideGrid) - 1;
+            console.log('maxIndex', maxIndex);
+
             setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
         }
     };
     const toggleLeaving = () => setLeaving(false);
-    console.log(isLoading);
-    console.log(data);
+    const onBoxClicked = (movieId: number) => {
+        console.log(movieId);
+        navigate(`/movies/${movieId}`);
+    };
+
+    const handleResize = () => {
+        setWinW(window.innerWidth);
+    };
+
+    const changeSlideGrid = () => {
+        if (winW > 1024) {
+            setSlideGrid(6);
+        } else if (winW > 640) {
+            setSlideGrid(4);
+        } else if (winW <= 640) {
+            setSlideGrid(3);
+        }
+    };
+
+    const rowVariants = {
+        hidden: {
+            x: winW + 10,
+        },
+        visible: {
+            x: 0,
+        },
+        exit: {
+            x: -winW - 10,
+        },
+    };
+
+    useEffect(() => {
+        changeSlideGrid();
+        increaseIndex();
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.addEventListener('resize', handleResize);
+        };
+    }, [winW]);
 
     return (
         <Wrapper className="root_section">
@@ -113,12 +193,24 @@ function Home() {
                         {/* onExitComplete 애니메이션이 끝난 후 함수 호출 */}
                         {/* initial 초기 화면이 애니메이션 값이 들어온 후로 보여지게 만들어 줌 */}
                         <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
-                            <Row variants={rowVariants} initial="hidden" animate="visible" exit="exit" transition={{ type: 'tween', duration: 1 }} key={index}>
+                            <Row variants={rowVariants} initial="hidden" animate="visible" exit="exit" transition={{ type: 'tween', duration: 1 }} key={index} $grid={slideGrid}>
                                 {data?.results
                                     .slice(1)
-                                    .slice(offset * index, offset * index + offset)
+                                    .slice(slideGrid * index, slideGrid * index + slideGrid)
                                     .map((movie) => (
-                                        <Box key={movie.id} $bgPhoto={makeImagePath(movie.backdrop_path, 'w500')}></Box>
+                                        <Box
+                                            onClick={() => onBoxClicked(movie.id)}
+                                            variants={boxVariants}
+                                            initial="normal"
+                                            whileHover="hover"
+                                            transition={{ type: 'tween' }}
+                                            key={movie.id}
+                                            $bgPhoto={makeImagePath(movie.backdrop_path, 'w500')}
+                                        >
+                                            <Info variants={infoVariants}>
+                                                <h4>{movie.title}</h4>
+                                            </Info>
+                                        </Box>
                                     ))}
                             </Row>
                         </AnimatePresence>
